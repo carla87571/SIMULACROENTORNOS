d122747c03fd28a1b5cd83ddcba5ae5863d78a86
carla <carla87571@hotmail.com>
2024-01-17 15:13:09 +0100

    subida del jar

---
+++ Carla.java
@@ -0,0 +1,18 @@
+public class Carla {
+    public static void main(String[] args) {
+        System.out.println("Hola Mundo");
+
+        char letra = 'a';
+        String cadena = "Hola\n";
+        String cadena2 = cadena + "Salva";
+        boolean x = false;
+        double decimal = 0.2;
+        int entero = 0;
+        int entero2 = 8;
+        int suma = entero + entero2;
+        System.out.println("El resultado de la suma es: " +suma);
+        System.out.println(cadena + cadena2);
+
+
+    }
+}
---
+++ documentacion.md
@@ -0,0 +1,5 @@
+# Documentación de Carla
+Esta es la documentación principal para Carla.
+
+El programa imprime por pantalla “HolaMundo”.
+
